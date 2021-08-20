c99e72e6587aa09490a798cc5442658214029868
Your Name <you@example.com>
2021-08-20 17:34:23 +0900

    Third Commit

---
+++ javascript/app.js
@@ -0,0 +1,8 @@
+function show(){
+    var userName = 'Mg Mg';
+document.write('hello JS');
+console.log('hello JS');
+alert('hello JS');
+document.getElementById('hello').innerHTML='DOM hello JS'
+document.write(`<br> ${userName} lives in "Yangon."`)
+}
